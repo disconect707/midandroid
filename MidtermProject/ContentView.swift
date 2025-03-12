3c784b7680aa530ab604ce4970d80f584c674c02
Dias Manasbek <Manasbekdias4@gmail.com>
2025-03-12 16:35:44 +0500

    Добавил хобби и спорт с анимацией

--- MidtermProject/ContentView.swift
+++ MidtermProject/ContentView.swift
@@ -1,21 +1,49 @@
-//
-//  ContentView.swift
-//  MidtermProject
-//
-//  Created by dias on 12.03.2025.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            VStack(spacing: 20) {
+                Text("Обо мне")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+
+                NavigationLink(destination: HobbyView()) {
+                    MenuItem(icon: "star.fill", title: "Мои хобби")
+                }
+
+                NavigationLink(destination: SportView()) {
+                    MenuItem(icon: "figure.walk", title: "Мои спортивные увлечения")
+                }
+
+                Spacer()
+            }
+            .padding()
+            .navigationTitle("Главная")
+        }
+    }
+}
+
+struct MenuItem: View {
+    var icon: String
+    var title: String
+
+    var body: some View {
+        HStack {
+            Image(systemName: icon)
+                .resizable()
+                .frame(width: 30, height: 30)
+                .foregroundColor(.blue)
+            
+            Text(title)
+                .font(.title2)
+                .fontWeight(.semibold)
+
+            Spacer()
         }
         .padding()
+        .background(Color(.systemGray6))
+        .cornerRadius(10)
     }
 }
 
