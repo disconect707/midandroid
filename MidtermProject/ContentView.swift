f3cc5c5d9fa031825324883562d3972cdc5b7be6
Dias Manasbek <Manasbekdias4@gmail.com>
2025-03-12 15:59:02 +0500

    Initial Commit

---
+++ MidtermProject/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MidtermProject
+//
+//  Created by dias on 12.03.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MidtermProject/MidtermProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MidtermProjectApp.swift
+//  MidtermProject
+//
+//  Created by dias on 12.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct MidtermProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
